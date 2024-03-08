64ba20d703eb8ba49b1ccf153ebf87d67dd2954f
Peter Dinis <peterdinis0@pobox.sk>
2024-03-08 19:31:43 +0100

    FOO

---
+++ app/_appwrite/connect.ts
@@ -0,0 +1,7 @@
+import { Client } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as unknown as string)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT as unknown as string);
