12f6dd5981585aef9842d495a46714a2a2008bd6
Peter Dinis <peterdinis0@pobox.sk>
2024-03-09 09:30:48 +0100

    infra: Updating appwrite config

--- app/_appwrite/connect.ts
+++ app/_appwrite/connect.ts
@@ -1,7 +1,11 @@
-import { Client } from 'appwrite';
+import { Account, Client } from 'appwrite';
 
 const client = new Client();
 
 client
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as unknown as string)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT as unknown as string);
+
+export const account = new Account(client);
+export {ID} from "appwrite";
+export default client;
