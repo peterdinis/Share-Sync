69db4cf9e580392c80233a81ec667f4dfd6cbe09
Peter Dinis <peterdinis0@pobox.sk>
2024-03-10 09:20:24 +0100

    feat: Updating landing page WIP

--- app/_components/hero/HeroWrapper.tsx
+++ app/_components/hero/HeroWrapper.tsx
@@ -1,6 +1,4 @@
 import { FC } from 'react';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
 
 const HeroWrapper: FC = () => {
     return (
@@ -8,34 +6,38 @@ const HeroWrapper: FC = () => {
             <div className='container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between'>
                 <div className='flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left'>
                     <h1 className='text-5xl font-bold leadi sm:text-6xl'>
-                        Share
-                        <span className='dark:text-violet-400'>Sync</span>
+                        Ac mattis
+                        <span className='dark:text-violet-400'>senectus</span>
+                        erat pharetra
                     </h1>
                     <p className='mt-6 mb-8 text-lg sm:mb-12'>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Accusantium, officiis quia. Officiis ratione vel
-                        nam accusamus, incidunt quidem voluptas quae quam
-                        consequatur quod asperiores sit sunt dolorem maxime
-                        excepturi tempora necessitatibus velit sint accusantium
-                        numquam saepe fuga beatae esse. Voluptatem?
+                        Dictum aliquam porta in condimentum ac integer
+                        <br className='hidden md:inline lg:hidden' />
+                        turpis pulvinar, est scelerisque ligula sem
                     </p>
                     <div className='flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start'>
-                        <Button
-                            variant={'default'}
+                        <a
+                            rel='noopener noreferrer'
+                            href='#'
                             className='px-8 py-3 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900'
                         >
-                            <Link href='/login'>Try now</Link>
-                        </Button>
-                        <Button
-                            variant={'secondary'}
+                            Suspendisse
+                        </a>
+                        <a
+                            rel='noopener noreferrer'
+                            href='#'
                             className='px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100'
                         >
-                            <Link href='/register'>Register</Link>
-                        </Button>
+                            Malesuada
+                        </a>
                     </div>
                 </div>
                 <div className='flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128'>
-
+                    <img
+                        src='assets/svg/Business_SVG.svg'
+                        alt=''
+                        className='object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128'
+                    />
                 </div>
             </div>
         </section>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import HeroWrapper from './_components/hero/Wrapper';
+import HeroWrapper from './_components/hero/HeroWrapper';
 import Footer from './_components/shared/Footer';
 
 const Homepage: NextPage = () => {
