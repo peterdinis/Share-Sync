df24fe336ad959150ef540631961c48f053c93c9
Peter Dinis <peterdinis0@pobox.sk>
2024-03-10 09:22:43 +0100

    feat: Remove footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { NextPage } from 'next';
 import HeroWrapper from './_components/hero/HeroWrapper';
-import Footer from './_components/shared/Footer';
 
 const Homepage: NextPage = () => {
     return (
         <div className='mt-3'>
             <HeroWrapper />
-            <Footer />
         </div>
     );
 };
