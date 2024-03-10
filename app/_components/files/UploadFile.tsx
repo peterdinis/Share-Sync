58188256edd5f1beefd68fadfba0c340e54659f3
Peter Dinis <peterdinis0@pobox.sk>
2024-03-10 13:11:07 +0100

    style: Design upload modal file

--- app/_components/files/UploadFile.tsx
+++ app/_components/files/UploadFile.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 import UploadModal from './UploadModal';
+import Dropzone, { useDropzone } from 'react-dropzone';
+import { FiUpload } from 'react-icons/fi';
+import { Input } from '@/components/ui/input';
 
 interface IUploadFileProps {
     text?: string;
@@ -8,7 +11,12 @@ interface IUploadFileProps {
 const UploadFile: FC<IUploadFileProps> = ({ text }: IUploadFileProps) => {
     return (
         <>
-            <UploadModal modalText={text}></UploadModal>
+            <UploadModal modalText={text}>
+                <div className=' bg-black text-white flex items-center py-2 px-3 rounded-md hover:bg-opacity-80'>
+                    <FiUpload size='1.2em' />
+                    <span className=' ml-2 text-sm'>Upload Image</span>
+                </div>
+            </UploadModal>
         </>
     );
 };
