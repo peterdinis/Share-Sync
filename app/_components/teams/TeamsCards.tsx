4d1780863bb5505032ff28e8e29c3a2f6533d68c
Peter Dinis <peterdinis0@pobox.sk>
2024-03-11 18:00:50 +0100

    FOO

--- app/_components/teams/TeamsCards.tsx
+++ app/_components/teams/TeamsCards.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
@@ -11,87 +13,19 @@ import {
 import { Check } from "lucide-react";
 import { FC, useMemo } from "react";
 import { motion } from "framer-motion";
+import getScrollAnimation from "@/lib/scrollAnimation";
 
 enum PopularPlanType {
   NO = 0,
   YES = 1,
 }
 
-interface PricingProps {
-  title: string;
-  popular: PopularPlanType;
-  price: number;
-  description: string;
-  buttonText: string;
-  benefitList: string[];
-}
-
-const pricingList: PricingProps[] = [
-  {
-    title: "Free",
-    popular: 0,
-    price: 0,
-    description:
-      "Lorem ipsum dolor sit, amet ipsum consectetur adipisicing elit.",
-    buttonText: "Get Started",
-    benefitList: [
-      "1 Team member",
-      "2 GB Storage",
-      "Upto 4 pages",
-      "Community support",
-      "lorem ipsum dolor",
-    ],
-  },
-  {
-    title: "Premium",
-    popular: 1,
-    price: 5,
-    description:
-      "Lorem ipsum dolor sit, amet ipsum consectetur adipisicing elit.",
-    buttonText: "Start Free Trial",
-    benefitList: [
-      "4 Team member",
-      "4 GB Storage",
-      "Upto 6 pages",
-      "Priority support",
-      "lorem ipsum dolor",
-    ],
-  },
-  {
-    title: "Enterprise",
-    popular: 0,
-    price: 40,
-    description:
-      "Lorem ipsum dolor sit, amet ipsum consectetur adipisicing elit.",
-    buttonText: "Contact US",
-    benefitList: [
-      "10 Team member",
-      "8 GB Storage",
-      "Upto 10 pages",
-      "Priority support",
-      "lorem ipsum dolor",
-    ],
-  },
-];
-
-const Pricing: FC = () => {
+const TeamsCards: FC = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
 
   return (
     <motion.div variants={scrollAnimation}>
       <section id="pricing" className="container py-24 sm:py-32">
-        <h2 className="text-3xl md:text-4xl font-bold text-center">
-          Get
-          <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-            {" "}
-            Unlimited{" "}
-          </span>
-          Access
-        </h2>
-        <h3 className="text-xl text-center text-muted-foreground pt-4 pb-8">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-          reiciendis.
-        </h3>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {pricingList.map((pricing: PricingProps) => (
             <Card
@@ -143,4 +77,4 @@ const Pricing: FC = () => {
   );
 };
 
-export default Pricing;
+export default TeamsCards;
--- app/_components/teams/TeamsWrapper.tsx
+++ app/_components/teams/TeamsWrapper.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import Header from '../shared/Header';
+import TeamsCards from './TeamsCards';
 
 const TeamsWrapper: FC = () => {
     return (
         <div className='overflow-x-hidden ml-5 py-4 px-8 flex-grow  flex-1'>
             <Header text='My Teams' />
+            <TeamsCards />
         </div>
     );
 };
