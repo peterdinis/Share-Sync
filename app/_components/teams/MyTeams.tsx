b1e543ca542845930f337fad47dc46dd43b32b4a
Peter Dinis <peterdinis0@pobox.sk>
2024-03-11 17:23:37 +0100

    feat: Working on new temas component

--- app/_components/teams/MyTeams.tsx
+++ app/_components/teams/MyTeams.tsx
@@ -1,7 +1,12 @@
 import { FC } from 'react';
+import Sidebar from '../dashboard/sidebar/Sidebar';
 
 const MyTeams: FC = () => {
-    return <></>;
+    return (
+        <div className='flex text-gray-900 bg-gray-100 dark:bg-dark dark:text-light'>
+            <Sidebar />
+        </div>
+    )
 };
 
 export default MyTeams;
