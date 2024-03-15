c425997640c0dcd3f21fbdbebdbeba9f58e5e806
Peter Dinis <peterdinis0@pobox.sk>
2024-03-15 21:33:20 +0100

    FOO

--- app/_components/files/UploadFile.tsx
+++ app/_components/files/UploadFile.tsx
@@ -1,96 +1,76 @@
 'use client';
 
-import { FC, useCallback, useState } from 'react';
+import { FC, useState } from 'react';
 import UploadModal from './UploadModal';
-import Dropzone, { useDropzone } from 'react-dropzone';
-import { FiUpload } from 'react-icons/fi';
-import { Input } from '@/components/ui/input';
+import { uploadFile } from '@/app/_appwrite/services/uploadFunctions';
+import { toast } from "@/components/ui/use-toast";
 
 interface IUploadFileProps {
     text?: string;
 }
 
 const UploadFile: FC<IUploadFileProps> = ({ text }: IUploadFileProps) => {
-    const [loading, setLoading] = useState<boolean>(false);
-    const [progress, setProgress] = useState(0);
-    const onDrop = useCallback(async (acceptedFiles: File[]) => {
-        if (acceptedFiles.length > 0) {
-            const selectedImage = acceptedFiles[0];
-        }
-    }, []);
+    const [image, setImage] = useState<File | null>(null);
 
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({
-        onDrop,
-    });
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        if (image) {
+            try {
+                const response = await uploadFile(image);
+                console.log(response);
+                toast({
+                    duration: 2000,
+                    className: "bg-green-500",
+                    title: "File uploaded successfully"
+                });
+            } catch (error) {
+                console.error(error);
+                toast({
+                    duration: 2000,
+                    className: "bg-red-500",
+                    title: "Error uploading file"
+                });
+            }
+        } else {
+            toast({
+                duration: 2000,
+                className: "bg-red-500",
+                title: "No file selected"
+            });
+        }
+    };
+
     return (
         <>
             <UploadModal modalText={text}>
-                <form className='mt-8 space-y-3' action='#' method='POST'>
-                    <div className='grid grid-cols-1 space-y-2'>
-                        <label className='text-sm font-bold text-gray-500 tracking-wide'>
-                            Title
-                        </label>
-                        <input
-                            className='text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500'
-                            type=''
-                            placeholder='mail@gmail.com'
-                        />
-                    </div>
+                <form onSubmit={handleSubmit} className='mt-8 space-y-3'>
                     <div className='grid grid-cols-1 space-y-2'>
                         <label className='text-sm font-bold text-gray-500 tracking-wide'>
                             Attach Document
                         </label>
                         <div className='flex items-center justify-center w-full'>
                             <label className='flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center'>
-                                <div className='h-full w-full text-center flex flex-col items-center justify-center items-center  '>
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        className='w-10 h-10 text-blue-400 group-hover:text-blue-600'
-                                        fill='none'
-                                        viewBox='0 0 24 24'
-                                        stroke='currentColor'
-                                    >
-                                        <path
-                                            stroke-linecap='round'
-                                            stroke-linejoin='round'
-                                            stroke-width='2'
-                                            d='M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12'
-                                        />
-                                    </svg>
-                                    <div className='flex flex-auto max-h-48 w-2/5 mx-auto -mt-10'>
-                                        <img
-                                            className='has-mask h-36 object-center'
-                                            src='https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg?size=338&ext=jpg'
-                                            alt='freepik image'
-                                        />
-                                    </div>
-                                    <p className='pointer-none text-gray-500 '>
-                                        <span className='text-sm'>
-                                            Drag and drop
-                                        </span>{' '}
-                                        files here <br /> or{' '}
-                                        <a
-                                            href=''
-                                            id=''
-                                            className='text-blue-600 hover:underline'
-                                        >
-                                            select a file
-                                        </a>{' '}
-                                        from your computer
+                                <div className='h-full w-full text-center flex flex-col justify-center items-center'>
+                                    {/* SVG and Image placeholder */}
+                                    <p className='pointer-none text-gray-500'>
+                                        <span className='text-sm'>Drag and drop</span> files here <br /> or <a href='#' className='text-blue-600 hover:underline'>select a file</a> from your computer
                                     </p>
                                 </div>
-                                <input type='file' className='hidden' />
+                                <input 
+                                    onChange={(event) => setImage(event.target.files ? event.target.files[0] : null)} 
+                                    type='file' 
+                                    className='hidden' 
+                                />
                             </label>
                         </div>
                     </div>
                     <p className='text-sm text-gray-300'>
-                        <span>File type: doc,pdf,types of images</span>
+                        <span>File type: doc, pdf, types of images</span>
                     </p>
                     <div>
                         <button
                             type='submit'
-                            className='my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4  rounded-full tracking-wide
-                                    font-semibold  focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg cursor-pointer transition ease-in duration-300'
+                            className='my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg cursor-pointer transition ease-in duration-300'
                         >
                             Upload
                         </button>
